52d39394052c88c1a955737507b481d538e06d40
Vampiredk <simonbreit2@gmail.com>
2020-04-01 12:57:57 +0200

    Update myClass.cs

--- myClass.cs
+++ myClass.cs
@@ -2,6 +2,23 @@ namespace GitExercise
 {
     class myClass
     {
-        
+        public string _name;
+        public string _address;
+        public int _postNr;
+        public myClass(string Name, string Address, int postNr){
+            _name = Name;
+            _address = Address;
+            _postNr = postNr;
+        }
+
+        public string Name{
+            get {return _name; }
+        }
+        public string Address{
+            get {return _address; }
+        }
+        public int postNr{
+            get {return _postNr; }
+        }
     }
 }
